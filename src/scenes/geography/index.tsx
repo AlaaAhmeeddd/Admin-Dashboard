0de85fbb9ea584e4f930d7771b52f9c2dc879744
Your Name <alaaahmedsaber2002@gmail.com>
2024-12-02 07:24:52 +0200

    feat: add geography chart

--- src/scenes/geography/index.tsx
+++ src/scenes/geography/index.tsx
@@ -1,6 +1,21 @@
+import { Box } from "@mui/material"
+import GeographyChart from "../../components/GeographyChart"
+import Container from "../../components/Container"
+import Header from "../../components/Header"
 
-export default function Geography() {
+function index() {
   return (
-    <div>index</div>
+    <Container>
+      <Box className="overflow-x-hidden">
+        <Header title="Geography Chart" subTitle="Simple Geography Chart" />
+        <Box className="overflow-x-auto">
+          <Box className="border rounded-[4px] mt-8" height="75vh">
+            <GeographyChart />
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   )
 }
+
+export default index
