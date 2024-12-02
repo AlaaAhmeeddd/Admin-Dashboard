53520f5b8756ccd05472b9dc00106bfc2eca4aaf
Your Name <alaaahmedsaber2002@gmail.com>
2024-12-02 06:40:26 +0200

    feat: add line chart

--- src/scenes/line/index.tsx
+++ src/scenes/line/index.tsx
@@ -1,6 +1,21 @@
+import { Box } from "@mui/material"
+import LineChart from "../../components/LineChart"
+import Container from "../../components/Container"
+import Header from "../../components/Header"
 
-export default function Line() {
+function index() {
   return (
-    <div>index</div>
+    <Container>
+      <Box className="overflow-x-hidden">
+        <Header title="Line Chart" subTitle="Simple Line Chart" />
+        <Box className="overflow-x-auto">
+          <Box className="min-w-[500px]" height="75vh" >
+            <LineChart />
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   )
 }
+
+export default index
