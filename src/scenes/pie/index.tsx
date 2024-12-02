3795619924938fdd3f0d347151cfd2f0149ea6d5
Your Name <alaaahmedsaber2002@gmail.com>
2024-12-02 06:19:38 +0200

    feat: add pie chart

--- src/scenes/pie/index.tsx
+++ src/scenes/pie/index.tsx
@@ -1,6 +1,21 @@
+import { Box } from "@mui/material"
+import PieChart from "../../components/PieChart"
+import Container from "../../components/Container"
+import Header from "../../components/Header"
 
-export default function Pie() {
+function index() {
   return (
-    <div>index</div>
+    <Container>
+      <Box className="overflow-x-hidden">
+        <Header title="Pie Chart" subTitle="Simple Pie Chart" />
+        <Box className="overflow-x-auto">
+          <Box className="min-w-[500px]" height="75vh" >
+            <PieChart />
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   )
 }
+
+export default index
